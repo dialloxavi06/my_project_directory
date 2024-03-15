9ee0fc09db5b863ab38c6b43c271c1156aabb0aa
dialloxavi06 <107733204+dialloxavi06@users.noreply.github.com>
2024-03-15 07:32:15 +0100

    Update RecipeController

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Recipe;
 
 class RecipeController extends AbstractController
 {
 
     
     #[Route('/recette', name: 'recipe.index')]
-    public function index( Request $request, RecipeRepository $repository ): Response
+    public function index( Request $request, 
+    RecipeRepository $repository, 
+    EntityManagerInterface $em): Response
     {
         $recipes = $repository->findAll();
 
@@ -41,6 +45,15 @@ class RecipeController extends AbstractController
             
         ]);
       
+    }
+
+    #[Route('/recettes/{id}/edit', name: 'recipe.edit')]
+    public function edit(Recipe $recipe){
+
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe
+        ]);
+
     }
 
 }
